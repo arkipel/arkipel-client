707512910d9e0823697d0cfef6c3ac792ffe0306
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-08-27 22:52:42 -0400

    Add source code information

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,14 @@ class About extends React.PureComponent {
       <Fragment>
         <h1>About</h1>
         <p>Arkipel is a persistent browser-based game.</p>
+        <h2>Source code</h2>
+        <p>
+          The source code can be found on the{' '}
+          <a href="https://github.com/arkipel/arkipel-client">
+            project's GitHub repository
+          </a>
+          . It has a GPL-3.0 license.
+        </p>
       </Fragment>
     );
   }
