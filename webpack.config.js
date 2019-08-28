238d33f14f586b38002ff81a9f544c70b6ca6ac0
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-08-27 23:13:31 -0400

    Try something

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,18 @@ module.exports = {
         test: /\.(gif|jpg|png|svg)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
 
