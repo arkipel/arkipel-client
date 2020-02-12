bc60e5086058b19af16a0fd2b69428cbbbf48172
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-02-11 21:41:03 -0500

    Move Arkipel description to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,36 @@
 import React, { Fragment } from 'react';
 
+import arkipelLogo256 from '../assets/logo/arkipel_256.png';
+
 class Home extends React.PureComponent {
   render() {
     return (
       <Fragment>
         <h1>Home</h1>
+        <div style={{ textAlign: 'center' }}>
+          <img
+            src={arkipelLogo256}
+            alt="Arkipel logo (256 pixels)"
+            height={80}
+            width={80}
+          />
+        </div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <h2>Description</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <i>
+            Arkipel is a persistent browser-based game that takes place in a
+            fictional archipelago called Arkipel. Players buy and sell land,
+            build infrastructure, trade resources and currencies, invest in
+            research, and handle the politics while keeping the population under
+            control in order to accumulate wealth and power.
+          </i>
         </p>
         <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolorem eum fugiat quo voluptas nulla pariatur?
-        </p>
-        <h2>More details</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <i>
+            Anything that players want to trade can be traded, such as land,
+            money, resources, votes, debt, etc. When a new account is created,
+            nothing is given for free. A new player should seek help from an
+            already existing player.
+          </i>
         </p>
       </Fragment>
     );
