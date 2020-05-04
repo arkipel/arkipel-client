72c6465c41f8755a2dda32a22d2370a1400c5a42
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-05-03 22:45:20 -0400

    Save

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,8 @@ import menu from './assets/icons/menu.png';
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    // uri: 'https://api.arkipel.io/query',
-    uri: 'http://local.arkipel.io:9192/query',
+    uri: 'https://api.arkipel.io/query',
+    // uri: 'http://local.arkipel.io:9192/query',
     credentials: 'include',
   }),
 });
