4e7accdc0d7612cebd57f369cec533bcc2bbcaca
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-05-04 22:10:33 -0400

    Save

--- src/pages/islands/List.tsx
+++ src/pages/islands/List.tsx
@@ -1,62 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import { Client } from '../../libs/jsonapi/client';
+import { useQuery, gql } from '@apollo/client';
 
-import { Island } from '../../models/Island';
+const IslandsList = () => {
+  // const islands = useState<Array<Island>>();
 
-class IslandsList extends React.PureComponent<props, state> {
-  constructor(props: props) {
-    super(props);
+  const { data, loading, error } = useQuery(
+    gql`
+      query listOfIslands {
+        islands {
+          name
+        }
+      }
+    `,
+  );
 
-    let client = new Client('http://127.0.0.1:9192');
-
-    client.schema.addType(new Island());
-
-    // Get islands
-    let req = client.getMany<Island>({ type: 'islands' });
-    req.then((islands) => {
-      this.setState({ islands });
-    });
-
-    this.state = {
-      islands: new Array<Island>(),
-    };
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <h1>Islands</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Population</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.islands.map((island) => {
-              return (
-                <tr key={island.id}>
-                  <th>
-                    <NavLink to={'islands/' + island.id}>{island.name}</NavLink>
-                  </th>
-                  <td>0</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </Fragment>
-    );
-  }
-}
-
-type props = {};
-
-type state = {
-  islands: Array<Island>;
+  return (
+    <Fragment>
+      <h1>Islands</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Population</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {islands.map((island) => {
+            return (
+              <tr key={island.id}>
+                <th>
+                  <NavLink to={'islands/' + island.id}>{island.name}</NavLink>
+                </th>
+                <td>0</td>
+              </tr>
+            );
+          })} */}
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default IslandsList;
