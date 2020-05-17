cf67d3ec644a81a05f3b74d72106fb8d52615586
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-05-17 14:10:01 -0400

    Save

--- src/libs/session/session.tsx
+++ src/libs/session/session.tsx
@@ -1,28 +1,10 @@
 import React, { FunctionComponent, useState } from 'react';
 import { useCookies } from 'react-cookie';
 
-import { gql, useApolloClient, ApolloClient } from '@apollo/client';
-
-class Session {
-  loggedIn = false;
-  id = '';
-  username = '';
-  logIn = (_: string) => {};
-  logOut = () => {};
-}
-
-const SessionContext = React.createContext(new Session());
-
 const SessionProvider: FunctionComponent = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [id, setId] = useState('');
-  const [username, setUsername] = useState('');
+  const [session, setSession] = useState(new Session(''));
 
   const [cookies, setCookie, removeCookie] = useCookies(['session']);
-
-  const client = useApolloClient();
-
-  let session = new Session();
 
   if (cookies.session && cookies.session.length > 0) {
     let data = JSON.parse(atob(cookies.session.split('.')[1]));
@@ -30,52 +12,37 @@ const SessionProvider: FunctionComponent = ({ children }) => {
     session.loggedIn = true;
     session.id = data.id;
     session.username = data.username;
-
-    getPersonalProfile(client, session.id, (result: any): void => {
-      if (result) {
-        if (result.data.me.__typename === 'User') {
-          setLoggedIn(true);
-          setId(result.data.me.id);
-          setUsername(result.data.me.username);
-        }
-      }
-    });
   }
 
   return (
     <SessionContext.Provider
       value={{
-        loggedIn,
-        id,
-        username,
+        ...session,
 
         logIn: (token: string) => {
-          if (!token || token === '') {
-            return;
-          }
-
-          setLoggedIn(false);
-          setId('');
-          setUsername('');
           removeCookie('session', {
             domain: '.arkipel.io',
+            path: '/',
           });
 
           setCookie('session', token, {
             domain: '.arkipel.io',
+            path: '/',
             maxAge: 60 * 60, // 1 hour
             // httpOnly: true,
             // sameSite: 'strict',
           });
+
+          setSession(new Session(token));
         },
 
         logOut: () => {
-          setLoggedIn(false);
-          setId('');
-          setUsername('');
           removeCookie('session', {
             domain: '.arkipel.io',
+            path: '/',
           });
+
+          setSession(new Session(''));
         },
       }}
     >
@@ -84,29 +51,47 @@ const SessionProvider: FunctionComponent = ({ children }) => {
   );
 };
 
-const getPersonalProfile = (
-  client: ApolloClient<object>,
-  userID: String,
-  cb: (result: any) => void,
-) => {
-  client
-    .query({
-      query: gql`
-        query me($userID: String!) {
-          me(userID: $userID) {
-            __typename
-            ... on User {
-              username
-              groups {
-                id
-              }
-            }
-          }
-        }
-      `,
-      variables: { userID },
-    })
-    .then(cb);
-};
+// const getPersonalProfile = (
+//   client: ApolloClient<object>,
+//   userID: string,
+//   cb: (result: any) => void,
+// ) => {
+//   client
+//     .query({
+//       query: gql`
+//         query me($userID: String!) {
+//           me(userID: $userID) {
+//             __typename
+//             ... on User {
+//               username
+//               groups {
+//                 id
+//               }
+//             }
+//           }
+//         }
+//       `,
+//       variables: { userID },
+//     })
+//     .then(cb);
+// };
+
+class Session {
+  constructor(token: string) {
+    if (token === '') {
+      return;
+    }
+
+    this.loggedIn = true;
+  }
+
+  loggedIn = false;
+  id = '';
+  username = '';
+}
+
+const SessionContext = React.createContext<
+  Session | { logIn: (_: string) => void; logOut: () => void }
+>({ logIn: (_: string) => {}, logOut: () => {} });
 
 export { SessionContext, SessionProvider, Session };
