fd24010bf114f30f6acd4e9a68352e5ee184393e
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-05-17 16:42:21 -0400

    Decouple some inputs out of the registration form

---
+++ src/components/passwordInput.tsx
@@ -0,0 +1,92 @@
+import React, { Fragment, useState, FunctionComponent, useEffect } from 'react';
+
+const UsernameInput: FunctionComponent<{
+  disabled?: boolean;
+  onUpdate?: (data: { password: string; valid: boolean }) => void;
+}> = ({ disabled, onUpdate }) => {
+  const [password, setPassword] = useState('');
+  const [passwordAgain, setPasswordAgain] = useState('');
+
+  // Inputs
+  let { passwordErrors, passwordAgainErrors } = check(password, passwordAgain);
+
+  useEffect(() => {
+    if (onUpdate) {
+      onUpdate({
+        password,
+        valid: passwordErrors.length === 0 && passwordAgainErrors.length === 0,
+      });
+    }
+  });
+
+  return (
+    <Fragment>
+      <p>
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          disabled={disabled || false}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        {passwordErrors.length > 0 && (
+          <Fragment>
+            <br />
+            <span className="hint-error">{passwordErrors.join(', ')}</span>
+          </Fragment>
+        )}
+        <br />
+        <span className="hint">at least 8 characters</span>
+      </p>
+      <p>
+        <input
+          type="password"
+          value={passwordAgain}
+          placeholder="Password again"
+          disabled={disabled || false}
+          onChange={(event) => {
+            setPasswordAgain(event.target.value);
+          }}
+        />
+        {passwordAgainErrors.length > 0 && (
+          <Fragment>
+            <br />
+            <span className="hint-error">{passwordAgainErrors.join(', ')}</span>
+          </Fragment>
+        )}
+        <br />
+        <span className="hint">same password</span>
+      </p>
+    </Fragment>
+  );
+};
+
+const check = (password: string, passwordAgain: string) => {
+  // Check password
+  let passwordErrors = new Array<string>();
+  if (password.length > 0 && password.length < 8) {
+    passwordErrors.push('too short');
+    passwordErrors.push('too short');
+  }
+
+  let passwordAgainErrors = new Array<string>();
+  if (passwordAgain.length > 0 && passwordAgain !== password) {
+    passwordAgainErrors.push('not the same');
+  }
+
+  // Allow subtmitting or not
+  let allowSubmit = false;
+  if (passwordErrors.length === 0 && passwordAgainErrors.length === 0) {
+    allowSubmit = true;
+  }
+
+  return {
+    passwordErrors,
+    passwordAgainErrors,
+    allowSubmit,
+  };
+};
+
+export default UsernameInput;
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,24 +1,21 @@
 import React, { Fragment, useState } from 'react';
 
-import { useQuery, gql, useMutation } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 
+import UsernameInput from '../components/usernameInput';
+import PasswordInput from '../components/passwordInput';
+
 const Registration = () => {
   const [username, setUsername] = useState('');
+  const [usernameIsValid, setUsernameIsValid] = useState(false);
   const [password, setPassword] = useState('');
-  const [passwordAgain, setPasswordAgain] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
   const [captcha, setCaptcha] = useState('');
 
-  // Inputs
-  let {
-    usernameErrors,
-    passwordErrors,
-    passwordAgainErrors,
-    allowSubmit,
-  } = checkInputs(username, password, passwordAgain);
-
-  if (captcha === '') {
+  let allowSubmit = true;
+  if (!usernameIsValid || !passwordIsValid || captcha === '') {
     allowSubmit = false;
   }
 
@@ -69,66 +66,20 @@ const Registration = () => {
           submit({ variables: { username, password, captcha } });
         }}
       >
-        <p>
-          <input
-            type="text"
-            value={username}
-            placeholder="Username"
-            maxLength={20}
-            disabled={registered}
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          {usernameErrors.length > 0 && (
-            <Fragment>
-              <br />
-              <span className="hint-error">{usernameErrors.join(', ')}</span>
-            </Fragment>
-          )}
-          <br />
-          <span className="hint">a-z, A-Z, 0-9, 4-20 characters</span>
-        </p>
-        <p>
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            disabled={registered}
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-          {passwordErrors.length > 0 && (
-            <Fragment>
-              <br />
-              <span className="hint-error">{passwordErrors.join(', ')}</span>
-            </Fragment>
-          )}
-          <br />
-          <span className="hint">at least 8 characters</span>
-        </p>
-        <p>
-          <input
-            type="password"
-            value={passwordAgain}
-            placeholder="Password again"
-            disabled={registered}
-            onChange={(event) => {
-              setPasswordAgain(event.target.value);
-            }}
-          />
-          {passwordAgainErrors.length > 0 && (
-            <Fragment>
-              <br />
-              <span className="hint-error">
-                {passwordAgainErrors.join(', ')}
-              </span>
-            </Fragment>
-          )}
-          <br />
-          <span className="hint">same password</span>
-        </p>
+        <UsernameInput
+          disabled={registered}
+          onUpdate={({ username, valid }) => {
+            setUsername(username);
+            setUsernameIsValid(valid);
+          }}
+        />
+        <PasswordInput
+          disabled={registered}
+          onUpdate={({ password, valid }) => {
+            setPassword(password);
+            setPasswordIsValid(valid);
+          }}
+        />
         <HCaptcha
           // sitekey="10000000-ffff-ffff-ffff-000000000001"
           sitekey="36cde9f3-38a3-4fd7-9314-bac28f55545b"
@@ -156,70 +107,6 @@ const Registration = () => {
         )}
     </Fragment>
   );
-};
-
-const checkInputs = (
-  username: string,
-  password: string,
-  passwordAgain: string,
-) => {
-  // Check username
-  let usernameErrors = new Array<string>();
-  if (username.length > 0 && username.length < 4) {
-    usernameErrors.push('too short');
-  } else if (username.length > 20) {
-    usernameErrors.push('too long');
-  }
-  if (username.match(/[^a-zA-Z0-9]+/)) {
-    usernameErrors.push('invalid characters');
-  }
-
-  // Check username availability
-  let { data, loading } = useQuery(
-    gql`
-      query checkUsernameAvailability($username: String!) {
-        checkUsernameAvailability(username: $username)
-      }
-    `,
-    { variables: { username }, skip: usernameErrors.length > 0 },
-  );
-
-  if (username.length > 0 && usernameErrors.length === 0) {
-    if (loading) {
-      usernameErrors.push('checking availability...');
-    } else if (data && !data.checkUsernameAvailability) {
-      usernameErrors.push('already taken');
-    }
-  }
-
-  // Check password
-  let passwordErrors = new Array<string>();
-  if (password.length > 0 && password.length < 8) {
-    passwordErrors.push('too short');
-    passwordErrors.push('too short');
-  }
-
-  let passwordAgainErrors = new Array<string>();
-  if (passwordAgain.length > 0 && passwordAgain !== password) {
-    passwordAgainErrors.push('not the same');
-  }
-
-  // Allow subtmitting or not
-  let allowSubmit = false;
-  if (
-    usernameErrors.length === 0 &&
-    passwordErrors.length === 0 &&
-    passwordAgainErrors.length === 0
-  ) {
-    allowSubmit = true;
-  }
-
-  return {
-    usernameErrors,
-    passwordErrors,
-    passwordAgainErrors,
-    allowSubmit,
-  };
 };
 
 export default Registration;
