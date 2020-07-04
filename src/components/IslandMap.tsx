52475b82c3703b0d963348fee0c6df4a82f59bc6
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-07-04 10:09:41 -0400

    Save

---
+++ src/components/IslandMap.tsx
@@ -0,0 +1,61 @@
+import React, { Fragment, FunctionComponent } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import styles from './IslandMap.scss';
+
+import Island from '../models/Island';
+
+const IslandMap: FunctionComponent<props> = ({ island }) => {
+  const history = useHistory();
+
+  let islandId = island.id || '';
+  let dna = island.dna;
+
+  if (!dna) {
+    dna = '0'.repeat(400);
+  }
+
+  let map = new Array<JSX.Element>();
+  for (let i = 0; i < dna.length; i++) {
+    const t = dna[i];
+
+    // Tile kind
+    let kind = 'deepWater';
+    switch (t) {
+      case '1':
+        kind = 'water';
+        break;
+      case '2':
+        kind = 'sand';
+        break;
+      case '3':
+        kind = 'land';
+        break;
+    }
+
+    let className = styles[kind];
+
+    map.push(
+      <div
+        key={Math.random()}
+        className={className}
+        onClick={() => {
+          history.push('/archipelago/islands/' + islandId + '/tiles/' + i);
+        }}
+      ></div>,
+    );
+  }
+
+  return (
+    <Fragment>
+      <h2>Map</h2>
+      <div className={styles.island}>{map}</div>
+    </Fragment>
+  );
+};
+
+class props {
+  island: Island = new Island({});
+}
+
+export default IslandMap;
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,84 @@
+import React, { Fragment, FunctionComponent } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Media from 'react-media';
+
+import { SessionContext } from '../libs/session/session';
+
+// Pages
+import About from '../pages/About';
+import Home from '../pages/Home';
+import IslandPage from '../pages/islands/Island';
+import IslandsPage from '../pages/islands/List';
+import Login from '../pages/Login';
+import Registration from '../pages/Registration';
+import Settings from '../pages/account/Settings';
+
+// Assets
+import appStyles from '../App.scss';
+import styles from './MainContent.scss';
+import topBarStyles from '../styles/top-bar.scss';
+
+const MainContent: FunctionComponent<props> = ({ onMenuOpen }) => {
+  return (
+    <div className={styles.main}>
+      <div className={topBarStyles.topBar + ' ' + topBarStyles.topBarRight}>
+        <SessionContext.Consumer>
+          {(session) => {
+            return (
+              <Fragment>
+                <div>
+                  <Media
+                    query="(max-width: 699px)"
+                    render={() => (
+                      <div onClick={onMenuOpen} className="button">
+                        <img
+                          src="https://icons.arkipel.io/ui/menu.svg"
+                          alt="&#10092;"
+                        />
+                      </div>
+                    )}
+                  />
+                  {session.loggedIn && <span>{session.username}</span>}
+                </div>
+                <div>
+                  {session.loggedIn && (
+                    <button
+                      onClick={() => {
+                        session.logOut();
+                      }}
+                    >
+                      Log out
+                    </button>
+                  )}
+                </div>
+              </Fragment>
+            );
+          }}
+        </SessionContext.Consumer>
+      </div>
+      <div className={appStyles.scrollable}>
+        <div className={styles.content}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/registration" exact component={Registration} />
+            <Route path="/account/settings" exact component={Settings} />
+            <Route path="/archipelago/islands" exact component={IslandsPage} />
+            <Route
+              path="/archipelago/islands/:islandID"
+              component={IslandPage}
+            />
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+type props = {
+  // visible: boolean;
+  onMenuOpen: () => void;
+};
+
+export default MainContent;
