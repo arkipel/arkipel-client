f8775c977c88ff36875731a6d4b55f80505c107d
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-07-12 07:08:03 -0400

    Add MapTile component

--- src/components/IslandMap.tsx
+++ src/components/IslandMap.tsx
@@ -1,68 +1,29 @@
 import React, { Fragment, FunctionComponent } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import MapTile from './MapTile';
+
 import styles from './IslandMap.scss';
 
 import Island from '../models/Island';
-import { Infrastructure } from '../generated/globalTypes';
 
 const IslandMap: FunctionComponent<props> = ({ island }) => {
   const history = useHistory();
 
   let islandId = island.id || '';
-  let dna = island.dna;
 
-  if (!dna) {
-    dna = '0'.repeat(400);
-  }
-
-  let map = new Array<JSX.Element>();
-  for (let i = 0; i < dna.length; i++) {
-    const t = dna[i];
-
-    // Tile kind
-    let kind = 'deepWater';
-    switch (t) {
-      case '1':
-        kind = 'water';
-        break;
-      case '2':
-        kind = 'sand';
-        break;
-      case '3':
-        kind = 'land';
-        break;
-    }
-
-    let className = styles[kind];
-
-    let tile = island.tiles[i];
-
-    let infraIcon = <></>;
-    if (tile) {
-      switch (tile.infrastructure) {
-        case Infrastructure.FOREST:
-          infraIcon = (
-            <img src="https://icons.arkipel.io/infra/jungle.svg" alt="Forest" />
-          );
-          break;
-        default:
-          break;
-      }
-    }
-
-    map.push(
-      <div
+  let map = island.tiles.map((tile, i) => {
+    return (
+      <MapTile
         key={Math.random()}
-        className={className}
+        tile={tile}
+        clickable={islandId !== ''}
         onClick={() => {
           history.push('/archipelago/islands/' + islandId + '/tiles/' + i);
         }}
-      >
-        {infraIcon}
-      </div>,
+      />
     );
-  }
+  });
 
   return (
     <Fragment>
--- src/models/Island.ts
+++ src/models/Island.ts
@@ -1,13 +1,25 @@
 import Tile from '../models/Tile';
 
+import { TileKind } from '../generated/globalTypes';
+
 class Island {
   constructor(obj: any) {
     this.id = obj?.id || '';
     this.name = obj?.name || '';
-    this.dna = obj?.dna || '';
+    this.dna = obj?.dna || '0'.repeat(400);
     this.active = obj?.active || '';
 
+    // Tiles
     this.tiles = obj?.tiles || new Array<Tile>(400);
+    if (obj?.tiles) {
+      this.tiles = obj?.tiles;
+    } else {
+      for (let i = 0; i < this.dna.length; i++) {
+        this.tiles[i] = new Tile({
+          kind: TileKind.DEEP_WATER,
+        });
+      }
+    }
   }
 
   id: string;
