9b47e762023ae2aab7c5898b189d5bb5f2517130
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-07-13 19:08:17 -0400

    Save

--- src/pages/tile/Actions.tsx
+++ src/pages/tile/Actions.tsx
@@ -1,10 +1,32 @@
 import React, { Fragment } from 'react';
 
+import styles from './Actions.scss';
+
 const TileActions = () => {
   return (
     <Fragment>
       <h3>Actions</h3>
-      <p>Actions.</p>
+      <div className={styles.actionsBoard}>
+        <div className={styles.description}>
+          You may claim this tile at no cost. You may claim this tile at no
+          cost. You may claim this tile at no cost.
+        </div>
+        <div className={styles.buttons}>
+          <button>Claim</button>
+        </div>
+        <div className={styles.description}>
+          You may <b>not</b> abandon this tile because you do not own it.
+        </div>
+        <div className={styles.buttons}>
+          <button disabled>Abandoning the tiling</button>
+        </div>
+        <div className={styles.description}>
+          You may <b>not</b> sell this tile because you do not own it.
+        </div>
+        <div className={styles.buttons}>
+          <button disabled>Sell</button>
+        </div>
+      </div>
     </Fragment>
   );
 };
