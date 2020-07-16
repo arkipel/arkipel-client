7abd0b77aeee0563c6cefb157846a794db8b3f61
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-07-15 22:54:44 -0400

    save

--- src/pages/tile/Actions.tsx
+++ src/pages/tile/Actions.tsx
@@ -1,34 +1,92 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, FunctionComponent, useState } from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import { gql, useMutation } from '@apollo/client';
+import { Register, RegisterVariables } from '../generated/Register';
+
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+
+import UsernameInput from '../components/usernameInput';
+import PasswordInput from '../components/passwordInput';
+
+import { Info, Success, Error } from '../ui/dialog/Msg';
 
 import styles from './Actions.scss';
 
-const TileActions = () => {
+const TileActions: FunctionComponent<props> = ({ islandId, position }) => {
+  // const [captcha, setCaptcha] = useState('');
+
+  const formFunctions = useForm({
+    mode: 'onChange',
+    criteriaMode: 'all',
+  });
+  const { formState } = formFunctions;
+
+  let allowSubmit = true;
+  if (!formState.isValid) {
+    allowSubmit = false;
+  } else if (captcha === '') {
+    allowSubmit = false;
+  }
+
+  // Get tile owner
+  let [submit, { loading, data }] = useMutation(
+    gql`
+      mutation getTileOwner($islandID: String!, $position: String!) {
+        getTile(islandID: $islandID, position: $position) {
+          __typename
+          ... on Tile {
+            owner {
+              id
+            }
+          }
+        }
+      }
+    `,
+  );
+
   return (
     <Fragment>
       <h3>Actions</h3>
-      <div className={styles.actionsBoard}>
-        <div className={styles.description}>
-          You may claim this tile at no cost. You may claim this tile at no
-          cost. You may claim this tile at no cost.
-        </div>
-        <div className={styles.buttons}>
-          <button>Claim</button>
-        </div>
-        <div className={styles.description}>
-          You may <b>not</b> abandon this tile because you do not own it.
-        </div>
-        <div className={styles.buttons}>
-          <button disabled>Abandoning the tiling</button>
-        </div>
-        <div className={styles.description}>
-          You may <b>not</b> sell this tile because you do not own it.
-        </div>
-        <div className={styles.buttons}>
-          <button disabled>Sell</button>
-        </div>
-      </div>
+      <FormProvider {...formFunctions}>
+        <form
+          onSubmit={formFunctions.handleSubmit((formData) => {
+            submit({
+              variables: {
+                username: formData.username,
+                password: formData.password,
+              },
+            });
+          })}
+        >
+          <UsernameInput disabled={registered} />
+          <PasswordInput disabled={registered} />
+          <HCaptcha
+            // sitekey="10000000-ffff-ffff-ffff-000000000001"
+            sitekey="36cde9f3-38a3-4fd7-9314-bac28f55545b"
+            onVerify={(c: string) => {
+              setCaptcha(c);
+            }}
+            onExpire={() => {
+              setCaptcha('');
+            }}
+          ></HCaptcha>
+          <p>
+            <input
+              type="submit"
+              value="Register"
+              disabled={!allowSubmit || loading}
+            />
+          </p>
+        </form>
+      </FormProvider>
     </Fragment>
   );
 };
+
+class props {
+  islandId: string = '';
+  position: number = 0;
+}
 
 export default TileActions;
