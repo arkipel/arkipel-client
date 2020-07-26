f1be07641039e95aa9fecdffe23e4972a377f694
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-07-26 11:18:28 -0400

    Use standard island layout instead of tile's kind

--- src/components/IslandMap.tsx
+++ src/components/IslandMap.tsx
@@ -6,24 +6,26 @@ import MapTile from './MapTile';
 import styles from './IslandMap.scss';
 
 import Island from '../models/Island';
+import Tile from '../models/Tile';
+import { TileKind } from '../generated/globalTypes';
 
 const IslandMap: FunctionComponent<props> = ({ island }) => {
   const history = useHistory();
 
-  let islandId = island.id || '';
-
-  let map = island.tiles.map((tile, i) => {
-    return (
+  let map = Array<any>(256);
+  for (let i = 0; i < 256; i++) {
+    map[i] = (
       <MapTile
-        key={Math.random()}
-        tile={tile}
-        clickable={islandId !== ''}
+        key={i}
+        kind={positionToKind(i)}
+        tile={island.tiles[i] || new Tile({ position: i })}
+        clickable={island.id !== ''}
         onClick={() => {
           history.push('/island/tiles/' + i);
         }}
       />
     );
-  });
+  }
 
   return (
     <Fragment>
@@ -37,3 +39,36 @@ class props {
 }
 
 export default IslandMap;
+
+const positionToKind = (pos: number): TileKind => {
+  let k = dna[pos];
+
+  switch (k) {
+    case '1':
+      return TileKind.WATER;
+    case '2':
+      return TileKind.SAND;
+    case '3':
+      return TileKind.LAND;
+    default:
+      return TileKind.DEEP_WATER;
+  }
+};
+
+const dna =
+  '0000000000000000' +
+  '0000011111100000' +
+  '0001112222111000' +
+  '0011222332221100' +
+  '0012233333322100' +
+  '0112333333332110' +
+  '0122333333332210' +
+  '0123333333333210' +
+  '0123333333333210' +
+  '0122333333332210' +
+  '0112333333332110' +
+  '0012233333322100' +
+  '0011222332221100' +
+  '0001112222111000' +
+  '0000011111100000' +
+  '0000000000000000';
--- src/models/Island.ts
+++ src/models/Island.ts
@@ -1,24 +1,11 @@
 import User from '../models/User';
 import Tile from '../models/Tile';
 
-import { TileKind } from '../generated/globalTypes';
-
 class Island {
   constructor(obj: any) {
     this.id = obj?.id || '';
     this.owner = obj?.owner || new User({});
-
-    // Tiles
     this.tiles = obj?.tiles || new Array<Tile>(256);
-    if (obj?.tiles) {
-      this.tiles = obj?.tiles;
-    } else {
-      for (let i = 0; i < 256; i++) {
-        this.tiles[i] = new Tile({
-          kind: TileKind.DEEP_WATER,
-        });
-      }
-    }
   }
 
   id: string;
