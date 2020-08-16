3888a87b9b9af084bb9e2716f1f331ceded7c575
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-08-16 17:34:47 -0400

    Add infrastructure icons

--- src/components/MapTile.tsx
+++ src/components/MapTile.tsx
@@ -33,11 +33,72 @@ const MapTile: FunctionComponent<props> = ({
   let infraIcon = <></>;
   if (tile.infrastructure) {
     switch (tile.infrastructure) {
-      case Infrastructure.FOREST:
+      case Infrastructure.JUNGLE:
         infraIcon = (
-          <img src="https://icons.arkipel.io/infra/jungle.svg" alt="Forest" />
+          <img src="https://icons.arkipel.io/infra/jungle.svg" alt="Jungle" />
         );
         break;
+      case Infrastructure.QUARRY:
+        infraIcon = (
+          <img src="https://icons.arkipel.io/infra/quarry.svg" alt="Quarry" />
+        );
+        break;
+      case Infrastructure.APARTMENTS:
+        infraIcon = (
+          <img
+            src="https://icons.arkipel.io/infra/apartments.svg"
+            alt="Apartments"
+          />
+        );
+        break;
+      case Infrastructure.HOUSE:
+        infraIcon = (
+          <img src="https://icons.arkipel.io/infra/house.svg" alt="House" />
+        );
+        break;
+      case Infrastructure.WHEAT_FIELD:
+        infraIcon = (
+          <img
+            src="https://icons.arkipel.io/infra/wheat_field.svg"
+            alt="Wheat field"
+          />
+        );
+        break;
+      case Infrastructure.ANIMAL_FARM:
+        infraIcon = (
+          <img
+            src="https://icons.arkipel.io/infra/animal_farm.svg"
+            alt="Animal farm"
+          />
+        );
+        break;
+      case Infrastructure.NUCLEAR_PLANT:
+        infraIcon = (
+          <img
+            src="https://icons.arkipel.io/infra/nuclear_plant.svg"
+            alt="Nuclear plant"
+          />
+        );
+        break;
+      case Infrastructure.WIND_TURBINE:
+        infraIcon = (
+          <img
+            src="https://icons.arkipel.io/infra/wind_turbine.svg"
+            alt="Wind turbine"
+          />
+        );
+        break;
+      case Infrastructure.PORT:
+        infraIcon = (
+          <img src="https://icons.arkipel.io/infra/port.svg" alt="Port" />
+        );
+        break;
+      case Infrastructure.BANK:
+        infraIcon = (
+          <img src="https://icons.arkipel.io/infra/bank.svg" alt="Bank" />
+        );
+        break;
+
       default:
         break;
     }
