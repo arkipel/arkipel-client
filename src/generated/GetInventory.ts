6724c51bba2c0d4bf027db899f94d932390c3645
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-08-27 22:28:38 -0400

    Generate code for handling Inventory from GetInventory query

--- src/generated/GetInventory.ts
+++ src/generated/GetInventory.ts
@@ -7,9 +7,21 @@
 // GraphQL query operation: GetInventory
 // ====================================================
 
-export interface GetInventory_inventory {
-  readonly __typename: 'Inventory' | 'NotAuthorized' | 'NotFound';
+export interface GetInventory_inventory_NotAuthorized {
+  readonly __typename: 'NotAuthorized' | 'NotFound';
 }
+
+export interface GetInventory_inventory_Inventory {
+  readonly __typename: 'Inventory';
+  readonly population: number;
+  readonly material: number;
+  readonly energyUsed: number;
+  readonly energy: number;
+}
+
+export type GetInventory_inventory =
+  | GetInventory_inventory_NotAuthorized
+  | GetInventory_inventory_Inventory;
 
 export interface GetInventory {
   readonly inventory: GetInventory_inventory;
