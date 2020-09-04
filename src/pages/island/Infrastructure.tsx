651422f29d0f99d022dbfe2c451c35297820d53e
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-09-03 22:31:55 -0400

    save

--- src/pages/island/Infrastructure.tsx
+++ src/pages/island/Infrastructure.tsx
@@ -1,10 +1,11 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, FunctionComponent, useContext } from 'react';
 
 import { useQuery, gql } from '@apollo/client';
 import { GetIsland, GetIslandVariables } from 'generated/GetIsland';
 
 import { SessionContext } from '../../libs/session/session';
 
+import Tile from '../../models/Tile';
 import Island from '../../models/Island';
 
 import MapTile from '../../components/MapTile';
@@ -61,44 +62,57 @@ const InfrastructurePage = () => {
       <h1>Infrastructure</h1>
       {loading && <p>Loading...</p>}
       <div className={styles.listTiles}>
-        {island.tiles.map((t, i) => {
-          return (
-            <div key={i} className={styles.tileBox}>
-              <div className={styles.tileLogo}>
-                <MapTile tile={t} clickable={false} onClick={() => {}} />
-              </div>
-              <div>
-                <div className={styles.stats}>
-                  <span>Level {t.level}</span>
-                  <div className={styles.iconStat}>
-                    <img
-                      // className={styles.materialIcon}
-                      src="https://icons.arkipel.io/res/population.svg"
-                    />
-                    <span>4</span>
-                  </div>
-                  <div className={styles.iconStat}>
-                    <img
-                      // className={styles.materialIcon}
-                      src="https://icons.arkipel.io/res/energy.svg"
-                    />
-                    <span>5</span>
-                  </div>
-                  <div className={styles.iconStat}>
-                    <img
-                      // className={styles.materialIcon}
-                      src="https://icons.arkipel.io/res/material.svg"
-                    />
-                    <span>20/s</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
+        {island.tiles.map((t) => {
+          return <InfrastructureItem key={t.position} tile={t} />;
         })}
       </div>
     </Fragment>
   );
 };
+
+const InfrastructureItem: FunctionComponent<props> = ({ tile }) => {
+  return (
+    <div className={styles.tileBox}>
+      <div className={styles.tileLogo}>
+        <MapTile tile={tile} />
+      </div>
+      <div className={styles.content}>
+        <div>
+          <span>
+            {tile.infrastructureName()} on {tile.kindName()}
+          </span>
+        </div>
+        <div className={styles.stats}>
+          <span>Level {tile.level}</span>
+          <div className={styles.iconStat}>
+            <img
+              // className={styles.materialIcon}
+              src="https://icons.arkipel.io/res/population.svg"
+            />
+            <span>4</span>
+          </div>
+          <div className={styles.iconStat}>
+            <img
+              // className={styles.materialIcon}
+              src="https://icons.arkipel.io/res/energy.svg"
+            />
+            <span>5</span>
+          </div>
+          <div className={styles.iconStat}>
+            <img
+              // className={styles.materialIcon}
+              src="https://icons.arkipel.io/res/material.svg"
+            />
+            <span>20/s</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+class props {
+  tile: Tile = new Tile({});
+}
 
 export default InfrastructurePage;
