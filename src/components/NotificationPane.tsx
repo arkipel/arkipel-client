108714d7303b7f7d9137d6a4c97a9603635967f7
Marc-François Cochaux-Laberge <contact@mfcl.io>
2020-09-07 12:03:11 -0400

    save

--- src/components/NotificationPane.tsx
+++ src/components/NotificationPane.tsx
@@ -1,5 +1,15 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import Media from 'react-media';
+
+import { useQuery, gql } from '@apollo/client';
+import {
+  GetAllConstructionSites,
+  GetAllConstructionSitesVariables,
+} from '../generated/GetAllConstructionSites';
+
+import Construction from '../models/ConstructionSite';
+
+import { SessionContext } from '../libs/session/session';
 
 // Components
 import Scrollable from '../ui/misc/Scrollable';
@@ -11,8 +21,44 @@ const NotificationPane: FunctionComponent<props> = ({
   visible,
   onCloseClick,
 }) => {
+  const session = useContext(SessionContext);
+
   let notificationPaneClassName = visible ? styles.visible + ' ' : '';
   notificationPaneClassName += styles.notificationPane;
+
+  let islandId = session.id;
+  const loggedIn = session.id !== '';
+
+  const { data, error } = useQuery<
+    GetAllConstructionSites,
+    GetAllConstructionSitesVariables
+  >(
+    gql`
+      query GetAllConstructionSites($islandId: String!) {
+        island(islandId: $islandId) {
+          ... on Island {
+            constructionSites {
+              infrastructure
+              workloadLeft
+              finishedAt
+              tile {
+                position
+              }
+            }
+          }
+        }
+      }
+    `,
+    { variables: { islandId } },
+  );
+
+  let sites = new Array<Construction>();
+  if (data?.island.__typename === 'Island') {
+    data.island.constructionSites.map((cs) => {
+      sites.push(new Construction(cs));
+    });
+  }
+  const hasSites = sites.length > 0;
 
   return (
     <div className={notificationPaneClassName}>
@@ -33,7 +79,20 @@ const NotificationPane: FunctionComponent<props> = ({
       </div>
       <Scrollable>
         <div className={styles.content}>
-          <p>This section is a work in progress.</p>
+          {!loggedIn && <p>You are not logged in.</p>}
+          {error && <p>Construction sites could not be loaded.</p>}
+          {loggedIn && !hasSites && <p>Nothing is currently being built.</p>}
+          {loggedIn &&
+            hasSites &&
+            sites.map((site) => {
+              return (
+                <p key={Math.random()}>
+                  A {site.infrastructure} is being built on tile{' '}
+                  {site.tilePosition}. It will be done{' '}
+                  <b>{site.finishedAt.toRelative()}</b>.
+                </p>
+              );
+            })}
         </div>
       </Scrollable>
     </div>
