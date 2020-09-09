0f2b56fef4883cd8d2b1e868f143b1e2bae095a9
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-09-08 20:29:06 -0400

    Convert the About component from class to hook

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,24 +1,22 @@
 import React, { Fragment } from 'react';
 
-class About extends React.PureComponent {
-  render() {
-    return (
-      <Fragment>
-        <h1>About</h1>
-        <p>
-          Arkipel is a game made by <a href="https://mfcl.io">mfcl</a>.
-        </p>
-        <h2>Source code</h2>
-        <p>
-          The source code of this app can be found on the{' '}
-          <a href="https://github.com/arkipel/arkipel-client">
-            project's GitHub repository
-          </a>
-          . It has a GPL-3.0 license.
-        </p>
-      </Fragment>
-    );
-  }
-}
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p>
+        Arkipel is a game made by <a href="https://mfcl.io">mfcl</a>.
+      </p>
+      <h2>Source code</h2>
+      <p>
+        The source code of this app can be found on the{' '}
+        <a href="https://github.com/arkipel/arkipel-client">
+          project's GitHub repository
+        </a>
+        . It has a GPL-3.0 license.
+      </p>
+    </Fragment>
+  );
+};
 
 export default About;
