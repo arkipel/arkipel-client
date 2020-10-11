b4c4c2edebfcaedbff574f8a6842dc4a97a74db4
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-10-11 16:30:38 -0400

    Add generated code for desired status mutation

---
+++ src/generated/SetInfrastructureDesiredStatus.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { InfrastructureStatus } from './globalTypes';
+
+// ====================================================
+// GraphQL mutation operation: SetInfrastructureDesiredStatus
+// ====================================================
+
+export interface SetInfrastructureDesiredStatus_setInfrastructureDesiredStatus_NotAuthorized {
+  readonly __typename: 'NotAuthorized' | 'NotFound';
+}
+
+export interface SetInfrastructureDesiredStatus_setInfrastructureDesiredStatus_Tile {
+  readonly __typename: 'Tile';
+  readonly id: string;
+  readonly desiredStatus: InfrastructureStatus;
+  readonly population: number;
+  readonly material: number;
+  readonly energy: number;
+}
+
+export type SetInfrastructureDesiredStatus_setInfrastructureDesiredStatus =
+  | SetInfrastructureDesiredStatus_setInfrastructureDesiredStatus_NotAuthorized
+  | SetInfrastructureDesiredStatus_setInfrastructureDesiredStatus_Tile;
+
+export interface SetInfrastructureDesiredStatus {
+  readonly setInfrastructureDesiredStatus: SetInfrastructureDesiredStatus_setInfrastructureDesiredStatus | null;
+}
+
+export interface SetInfrastructureDesiredStatusVariables {
+  readonly islandId: string;
+  readonly position: number;
+  readonly status: InfrastructureStatus;
+}
