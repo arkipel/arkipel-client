09da640d0d6763d5a01e07fd2632c95dba8fc6af
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-10-18 17:03:36 -0400

    Add Label component

---
+++ src/ui/text/Label.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from 'react';
+
+const Label: FunctionComponent<props> = ({
+  text,
+  textColor,
+  backgroundColor,
+}) => {
+  let style = {
+    padding: '2px 4px',
+    color: '',
+    background: '',
+    'border-radius': '2px',
+  };
+
+  if (textColor) {
+    style.color = textColor;
+  }
+
+  if (backgroundColor) {
+    style.background = backgroundColor;
+  }
+
+  return <span style={style}>{text}</span>;
+};
+
+interface props {
+  text: string;
+  textColor?: string;
+  backgroundColor?: string;
+}
+
+export default Label;
