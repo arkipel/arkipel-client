08361dfe188a7e1fc275d5b7bba10a3cb6059ae6
Marc-François Cochaux-Laberge <dev@mfcl.io>
2021-05-24 13:15:51 -0400

    Add trade events

--- src/generated/GetEvents.ts
+++ src/generated/GetEvents.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { EventsInput } from './globalTypes';
+import { EventsInput, CommodityType } from './globalTypes';
 
 // ====================================================
 // GraphQL query operation: GetEvents
@@ -13,11 +13,46 @@ export interface GetEvents_events_NotAuthorized {
   readonly __typename: 'NotAuthorized';
 }
 
-export interface GetEvents_events_EventList_events {
+export interface GetEvents_events_EventList_events_AccountCreation {
   readonly __typename: 'AccountCreation';
   readonly id: number;
   readonly happenedAt: any;
 }
+
+export interface GetEvents_events_EventList_events_SellOrderExecution_currency {
+  readonly __typename: 'Currency';
+  readonly code: string;
+}
+
+export interface GetEvents_events_EventList_events_SellOrderExecution {
+  readonly __typename: 'SellOrderExecution';
+  readonly id: number;
+  readonly happenedAt: any;
+  readonly currency: GetEvents_events_EventList_events_SellOrderExecution_currency;
+  readonly commodity: CommodityType;
+  readonly quantity: number;
+  readonly price: number;
+}
+
+export interface GetEvents_events_EventList_events_BuyOrderExecution_currency {
+  readonly __typename: 'Currency';
+  readonly code: string;
+}
+
+export interface GetEvents_events_EventList_events_BuyOrderExecution {
+  readonly __typename: 'BuyOrderExecution';
+  readonly id: number;
+  readonly happenedAt: any;
+  readonly currency: GetEvents_events_EventList_events_BuyOrderExecution_currency;
+  readonly commodity: CommodityType;
+  readonly quantity: number;
+  readonly price: number;
+}
+
+export type GetEvents_events_EventList_events =
+  | GetEvents_events_EventList_events_AccountCreation
+  | GetEvents_events_EventList_events_SellOrderExecution
+  | GetEvents_events_EventList_events_BuyOrderExecution;
 
 export interface GetEvents_events_EventList {
   readonly __typename: 'EventList';
