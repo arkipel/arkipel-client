c00ec4233a95c82b5505e2ce8101f04b51cf68b0
Marc-François Cochaux-Laberge <dev@mfcl.io>
2021-06-01 23:21:06 -0400

    Remove style related loaders

--- webpack.common.js
+++ webpack.common.js
@@ -30,26 +30,6 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                mode: 'local',
-                localIdentName: '[local]--[hash:base64:4]',
-              },
-            },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
         test: /\.(gif|jpg|png|svg)$/,
         use: ['file-loader'],
       },
