ef88e1a1391f9b05315dfe41d16a5a1aa861c7a7
Marc-François Cochaux-Laberge <dev@mfcl.io>
2021-06-23 23:42:47 -0400

    Redesign trade form

--- src/ui/form/Form.tsx
+++ src/ui/form/Form.tsx
@@ -2,21 +2,19 @@ import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
 const Form: FunctionComponent<{
+  className?: string;
   onSubmit?: () => void;
-}> = ({ children, onSubmit }) => {
-  return <StyledForm onSubmit={onSubmit}>{children}</StyledForm>;
+}> = ({ children, className, onSubmit }) => {
+  return (
+    <StyledForm className={className} onSubmit={onSubmit}>
+      {children}
+    </StyledForm>
+  );
 };
 
 const StyledForm = styled.form`
   display: grid;
   gap: 10px;
-
-  @media all and (max-width: 499px) {
-    button,
-    input {
-      width: 100%;
-    }
-  }
 `;
 
 export { Form };
--- src/ui/form/Input.tsx
+++ src/ui/form/Input.tsx
@@ -1,6 +1,11 @@
-import React, { forwardRef, InputHTMLAttributes } from 'react';
+import React, {
+  forwardRef,
+  InputHTMLAttributes,
+  SelectHTMLAttributes,
+} from 'react';
 import styled from 'styled-components';
 
+// Input
 const Input = React.forwardRef<HTMLInputElement, props>((props, ref) => {
   return <StyledInput ref={ref} {...props} />;
 });
@@ -8,9 +13,14 @@ const Input = React.forwardRef<HTMLInputElement, props>((props, ref) => {
 type props = InputHTMLAttributes<HTMLInputElement>;
 
 const StyledInput = styled.input`
+  width: 100%;
   padding: 6px;
   font-size: 18px;
   border: 1px solid black;
+
+  &:disabled {
+    background: #eee;
+  }
 `;
 
 // Submit (input)
@@ -23,6 +33,7 @@ const Submit = forwardRef<HTMLInputElement, submitProps>(
 type submitProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>;
 
 const StyledSubmit = styled.input`
+  width: 200px;
   padding: 6px;
   color: #fff;
   font-size: 18px;
@@ -32,7 +43,82 @@ const StyledSubmit = styled.input`
 
   &:disabled {
     background: rgba(80, 80, 80);
+    cursor: default;
+  }
+
+  @media all and (max-width: 499px) {
+    width: 100%;
   }
 `;
 
-export { Input, Submit };
+// Select
+const Select = forwardRef<HTMLSelectElement, selectProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <StyledSelect ref={ref} {...props}>
+        {children}
+      </StyledSelect>
+    );
+  },
+);
+
+type selectProps = SelectHTMLAttributes<HTMLSelectElement>;
+
+const StyledSelect = styled.select`
+  width: 100%;
+  padding: 6px;
+  color: #000;
+  font-size: 18px;
+  border: 1px solid black;
+  background: #fff;
+  cursor: pointer;
+
+  &:disabled {
+    background: #eee;
+  }
+`;
+
+// Radio
+const Radio = forwardRef<HTMLInputElement, radioProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <StyledRadio>
+        <Input type="radio" ref={ref} {...props} />
+        <span>{props.label}</span>
+      </StyledRadio>
+    );
+  },
+);
+
+interface radioProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  label: string;
+}
+
+const StyledRadio = styled.label`
+  display: block;
+  cursor: pointer;
+
+  span {
+    display: block;
+    padding: 6px;
+    text-align: center;
+    width: 100%;
+    border-bottom: 4px solid #eee;
+  }
+
+  input:checked ~ span {
+    border-bottom: 4px solid black;
+  }
+
+  &:disabled {
+    background: rgba(80, 80, 80);
+    cursor: default;
+  }
+
+  input {
+    display: none;
+  }
+`;
+
+export { Input, Submit, Select, Radio };
