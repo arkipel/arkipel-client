ced0063d7683d0ea63425df077361e55799cb08c
Marc-François Cochaux-Laberge <dev@mfcl.io>
2021-07-07 22:32:21 -0400

    Remove best offers from trade page

--- src/pages/market/Trade.tsx
+++ src/pages/market/Trade.tsx
@@ -1,18 +1,9 @@
-import React, {
-  Fragment,
-  useContext,
-  useState,
-  FunctionComponent,
-} from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 
 import { useQuery, useMutation, gql, useApolloClient } from '@apollo/client';
 import { SendOrder, SendOrderVariables } from '../../generated/SendOrder';
-import {
-  GetBestOffers,
-  GetBestOffersVariables,
-} from '../../generated/GetBestOffers';
 import {
   GetMyOpenOrders,
   GetMyOpenOrdersVariables,
@@ -324,12 +315,6 @@ const TradePage = () => {
           </Error>
         </div>
       </StyledForm>
-      <h2>Best offers</h2>
-      <BestOffers
-        side={orderParams.orderType === 'sell' ? OrderSide.SELL : OrderSide.BUY}
-        currencyId={orderParams.currencyId}
-        commodity={CommodityType.MATERIAL_1M}
-      />
       <h2>Open offers</h2>
       <OpenOffers />
     </Fragment>
@@ -364,119 +349,6 @@ const StyledForm = styled(Form)`
     grid-template-columns: 1fr;
   }
 `;
-
-const BestOffers: FunctionComponent<bestOffersProps> = (props) => {
-  const client = useApolloClient();
-
-  const input = {
-    side: props.side,
-    currencyId: props.currencyId,
-    commodity: props.commodity,
-  };
-
-  const { data, loading, error } = useQuery<
-    GetBestOffers,
-    GetBestOffersVariables
-  >(
-    gql`
-      query GetBestOffers($input: OrdersInput!) {
-        orders(input: $input) {
-          __typename
-          ... on OrderList {
-            orders {
-              id
-              side
-              expiresAt
-              currency {
-                id
-                code
-              }
-              commodity
-              quantity
-              price
-            }
-          }
-        }
-      }
-    `,
-    { variables: { input }, pollInterval: 10000 },
-  );
-
-  let offers = new Array<offer>();
-
-  if (data?.orders.__typename === 'OrderList') {
-    for (const o of data.orders.orders) {
-      offers.push({
-        id: o.id,
-        side: o.side,
-        expiresAt: DateTime.fromISO(o.expiresAt),
-        currencyCode: o.currency.code,
-        commodity: o.commodity,
-        quantity: o.quantity,
-        price: o.price,
-      });
-    }
-  }
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error || data?.orders.__typename !== 'OrderList') {
-    return <Error>Sorry, an error occurred.</Error>;
-  }
-
-  return (
-    <Fragment>
-      <p>
-        The following are the best offers that correspond to the commodity
-        selected above.
-      </p>
-      {offers.length === 0 && <Info>No offers found.</Info>}
-      {offers.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <th>Side</th>
-              <th>Qty</th>
-              <th>Commodity</th>
-              <th>Price</th>
-              <th>Time left</th>
-            </tr>
-          </thead>
-          <tbody>
-            {offers.map((offer) => {
-              return (
-                <tr key={offer.id}>
-                  <td>{offer.side}</td>
-                  <td>{offer.quantity}</td>
-                  <td>{commodityToString(offer.commodity)}</td>
-                  <td>{offer.price}</td>
-                  <td>
-                    <TimeLeft
-                      target={offer.expiresAt}
-                      onReach={() => {
-                        client.cache.evict({
-                          id: 'Offer:' + offer.id,
-                        });
-                      }}
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
-    </Fragment>
-  );
-};
-
-interface bestOffersProps {
-  side: OrderSide;
-  currencyId: string;
-  commodity: CommodityType;
-}
 
 const OpenOffers = () => {
   const session = useContext(SessionContext);
