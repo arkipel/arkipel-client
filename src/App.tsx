4bb4aa83d7be26dfef0854a4518380ec126a4d00
Marc-François Cochaux-Laberge <dev@mfcl.io>
2022-02-16 20:44:51 -0500

    Implement service worker

--- src/App.tsx
+++ src/App.tsx
@@ -68,6 +68,19 @@ const client = new ApolloClient({
   }),
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
+
 const App: FunctionComponent<props> = () => {
   const [showMenuPane, setShowMenuPane] = useState(false);
   const [showNotificationPane, setShowNotificationPane] = useState(false);
