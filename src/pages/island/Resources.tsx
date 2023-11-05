002f6b15fc45b5e05ed93a9960d8afcccacb1f83
Marc-François Cochaux-Laberge <contact@mfcl.ca>
2023-11-05 11:04:00 -0500

    Fix space given to numbers in Resources page

--- src/pages/island/Resources.tsx
+++ src/pages/island/Resources.tsx
@@ -16,96 +16,116 @@ const ResourcesPage = () => {
         <StatStyle>
           <div>
             <h3>Population</h3>
-            <p>Citizens live in houses or apartments and work on the island.</p>
+            <span>
+              {inventory.populationUsed}/{inventory.populationTotal}
+            </span>
           </div>
           <div>
-            {inventory.populationUsed}/{inventory.populationTotal}
+            <p>Citizens live in houses or apartments and work on the island.</p>
           </div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Builders</h3>
+            <span>{inventory.populationFree}</span>
+          </div>
+          <div>
             <p>
               Builders are citizens without a job and spend their time building
               new tiles. Having more of them means that new tiles get built more
               quickly.
             </p>
           </div>
-          <div>{inventory.populationFree}</div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Energy</h3>
-            <p>Energy is produced and consumed by tiles on the island.</p>
+            <span>
+              {inventory.energyUsed}/{inventory.energyTotal}
+            </span>
           </div>
           <div>
-            {inventory.energyUsed}/{inventory.energyTotal}
+            <p>Energy is produced and consumed by tiles on the island.</p>
           </div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Material production</h3>
+            <span>{inventory.materialProduction}/s</span>
+          </div>
+          <div>
             <p>
               The rate at which the island produces and accumulates resources.
             </p>
           </div>
-          <div>{inventory.materialProduction}/s</div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Material</h3>
+            <span>{FormatNumber(inventory.material)}</span>
+          </div>
+          <div>
             <p>
               Material is used to build new infrastructure on empty tiles or
               upgrade current infrastructure.
             </p>
           </div>
-          <div>{FormatNumber(inventory.material)}</div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Food production</h3>
+            <span>{inventory.foodProduction}/s</span>
+          </div>
+          <div>
             <p>The rate at which the island produces food.</p>
           </div>
-          <div>{inventory.foodProduction}/s</div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Food</h3>
+            <span>{FormatNumber(inventory.food)}</span>
+          </div>
+          <div>
             <p>
               Food is used to feed citizens. Every hour, 10% of the food stock
               goes bad and is thrown away. It currently has no impact in the
               game.
             </p>
           </div>
-          <div>{FormatNumber(inventory.food)}</div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Frozen food production</h3>
+            <span>{inventory.frozenFoodProduction}/s</span>
+          </div>
+          <div>
             <p>The rate at which the island freezes food.</p>
           </div>
-          <div>{inventory.frozenFoodProduction}/s</div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Food</h3>
+            <span>{FormatNumber(inventory.frozenFood)}</span>
+          </div>
+          <div>
             <p>
               Frozen food is food that does not rot, but requires a warehouse
               and energy to remain frozen. It currently has no impact in the
               game.
             </p>
           </div>
-          <div>{FormatNumber(inventory.frozenFood)}</div>
         </StatStyle>
         <StatStyle>
           <div>
             <h3>Frozen food storage</h3>
+            <span>{FormatNumber(inventory.frozenFoodStorage)}</span>
+          </div>
+          <div>
             <p>
               The maximum amount of frozen food that can be stored in the
               warehouses.
             </p>
           </div>
-          <div>{FormatNumber(inventory.frozenFoodStorage)}</div>
         </StatStyle>
       </Style>
       <h2>Infrastructure</h2>
@@ -113,12 +133,14 @@ const ResourcesPage = () => {
         <StatStyle>
           <div>
             <h3>Banking</h3>
+            <span>{inventory.bankLevels}</span>
+          </div>
+          <div>
             <p>
               More banking infrastructure gives the island more powerful tools
               to trade with the rest of the archipelago.
             </p>
           </div>
-          <div>{inventory.bankLevels}</div>
         </StatStyle>
       </Style>
     </Fragment>
@@ -131,21 +153,21 @@ const Style = styled.div`
 `;
 
 const StatStyle = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  grid-template-columns: 1fr 120px;
-  gap: 10px;
-
   div:nth-child(1) {
-    p {
-      font-size: 16px;
-      font-style: italic;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    span {
+      font-weight: bold;
     }
   }
 
   div:nth-child(2) {
-    font-size: 20px;
-    text-align: right;
+    p {
+      font-style: italic;
+      font-size: 16px;
+    }
   }
 `;
 
